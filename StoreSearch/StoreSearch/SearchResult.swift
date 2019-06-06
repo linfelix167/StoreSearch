595cc58c8b849f6734e231bfd664a14f2d666b2d
Felix Lin <swift.python.geeks@gmail.com>
2019-06-06 00:41:03 -0700

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Felix Lin on 6/5/19.
+//  Copyright © 2019 Felix Lin. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
