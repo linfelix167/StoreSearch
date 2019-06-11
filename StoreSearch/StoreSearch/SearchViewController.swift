b37e007d036ac13b823ec7c51b406a4ed7454653
Felix Lin <swift.python.geeks@gmail.com>
2019-06-11 01:44:29 -0700

    Add cancelling operations

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -28,6 +28,7 @@ class SearchViewController: UIViewController {
     var searchResults = [SearchResult]()
     var hasSearched = false
     var isLoading = false
+    var dataTask: URLSessionDataTask?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -84,6 +85,7 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if !searchBar.text!.isEmpty {
             searchBar.resignFirstResponder()
+            dataTask?.cancel()
             isLoading = true
             tableView.reloadData()
             
@@ -94,9 +96,9 @@ extension SearchViewController: UISearchBarDelegate {
             
             let session = URLSession.shared
             
-            let dataTask = session.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    print("Failure! \(error.localizedDescription)")
+            dataTask = session.dataTask(with: url) { data, response, error in
+                if let error = error as NSError?, error.code == -999 {
+                    return
                 } else if let httpResponse = response as? HTTPURLResponse,
                     httpResponse.statusCode == 200 {
                     if let data = data {
@@ -119,7 +121,7 @@ extension SearchViewController: UISearchBarDelegate {
                     self.showNetworkError()
                 }
             }
-            dataTask.resume()
+            dataTask?.resume()
         }
     }
     
