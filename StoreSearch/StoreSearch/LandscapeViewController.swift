c1d318a25518b104fdfc6069458effe380ea21c0
Felix Lin <swift.python.geeks@gmail.com>
2019-06-21 15:14:22 -0700

    Add landscape grid

--- StoreSearch/StoreSearch/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -12,6 +12,10 @@ class LandscapeViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
+    
+    var searchResults = [SearchResult]()
+    private var firstTime = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +30,8 @@ class LandscapeViewController: UIViewController {
         // Remove constraints for scroll view
         scrollView.removeConstraints(scrollView.constraints)
         scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
     }
     
     override func viewWillLayoutSubviews() {
@@ -38,5 +44,97 @@ class LandscapeViewController: UIViewController {
             width: safeFrame.size.width,
             height: pageControl.frame.size.height
         )
+        
+        if firstTime {
+            firstTime = false
+            tileButtons(searchResults)
+        }
+    }
+    
+    // MARK:- Private Methods
+    private func tileButtons(_ searchResults: [SearchResult]) {
+        var columnsPerPage = 6
+        var rowsPerPage = 3
+        var itemWidth: CGFloat = 94
+        var itemHeight: CGFloat = 88
+        var marginX: CGFloat = 2
+        var marginY: CGFloat = 20
+        
+        let viewWidth = scrollView.bounds.size.width
+        
+        switch viewWidth {
+        case 568:
+            // 4-inch device
+            break
+        case 667:
+            // 4.7-inch device
+            columnsPerPage = 7
+            itemWidth = 95
+            itemHeight = 98
+            marginX = 1
+            marginY = 29
+            
+        case 736:
+            // 5.5-inch device
+            columnsPerPage = 8
+            rowsPerPage = 4
+            itemWidth = 92
+            marginX = 0
+            
+        case 724:
+            // iPhone X
+            columnsPerPage = 8
+            rowsPerPage = 3
+            itemWidth = 90
+            itemHeight = 98
+            marginX = 2
+            marginY = 29
+            
+        default:
+            break
+        }
+        
+        // Button size
+        let buttonWidth: CGFloat = 82
+        let buttonHeight: CGFloat = 82
+        let paddingHorz = (itemWidth - buttonWidth) / 2
+        let paddingVert = (itemHeight - buttonHeight) / 2
+        
+        // Add the buttons
+        var row = 0
+        var column = 0
+        var x = marginX
+        for (index, result) in searchResults.enumerated() {
+            // 1
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+            // 2
+            button.frame = CGRect(
+                x: x + paddingHorz,
+                y: marginY + CGFloat(row) * itemHeight + paddingVert,
+                width: buttonWidth,
+                height: buttonHeight
+            )
+            // 3
+            scrollView.addSubview(button)
+            // 4
+            row += 1
+            if row == rowsPerPage {
+                row = 0; x += itemWidth; column += 1
+                
+                if column == columnsPerPage {
+                    column = 0; x += marginX * 2
+                }
+            }
+            // Set scroll view content size
+            let buttonsPerPage = columnsPerPage * rowsPerPage
+            let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+            scrollView.contentSize = CGSize(
+                width: CGFloat(numPages) * viewWidth,
+                height: scrollView.bounds.size.height
+            )
+            print("Number of pages: \(numPages)")
+        }
     }
 }
