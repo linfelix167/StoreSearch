03cec93f38cece4d3c275ff290217684f50f6e0b
Felix Lin <swift.python.geeks@gmail.com>
2019-07-19 11:30:53 -0700

    Representing the category as an enum

--- StoreSearch/StoreSearch/Controller/SearchViewController.swift
+++ StoreSearch/StoreSearch/Controller/SearchViewController.swift
@@ -129,7 +129,12 @@ class SearchViewController: UIViewController {
 extension SearchViewController: UISearchBarDelegate {
     
     fileprivate func performSearch() {
-        search.performSearch(for: searchBar.text!, category: segmentedControl.selectedSegmentIndex)
+        search.performSearch(for: searchBar.text!, category: segmentedControl.selectedSegmentIndex) { (success) in
+            if !success {
+                self.showNetworkError()
+            }
+            self.tableView.reloadData()
+        }
         
         tableView.reloadData()
         searchBar.resignFirstResponder()
--- StoreSearch/StoreSearch/Search.swift
+++ StoreSearch/StoreSearch/Search.swift
@@ -8,14 +8,24 @@
 
 import Foundation
 
+typealias SearchComplete = (Bool) -> Void
+
 class Search {
+    
+    enum Category: Int {
+        case all = 0
+        case music = 1
+        case software = 2
+        case ebooks = 3
+    }
+    
     var searchResults: [SearchResult] = []
     var hasSearched = false
     var isLoading = false
     
     private var dataTask: URLSessionDataTask? = nil
     
-    func performSearch(for text: String, category: Int) {
+    func performSearch(for text: String, category: Int, completion: @escaping SearchComplete) {
         if !text.isEmpty {
             dataTask?.cancel()
             
@@ -23,7 +33,35 @@ class Search {
             hasSearched = true
             searchResults = []
             
+            let url = iTunesURL(searchText: text, category: category)
             
+            let session = URLSession.shared
+            dataTask = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                var success = false
+                
+                if let error = error as NSError?, error.code == -999 {
+                    return
+                }
+                
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200, let data = data {
+                    self.searchResults = self.parse(data: data)
+                    self.searchResults.sort(by: <)
+                    
+                    print("Success!")
+                    self.isLoading = false
+                    success = true
+                }
+                
+                if !success {
+                    self.hasSearched = false
+                    self.isLoading = false
+                }
+                
+                DispatchQueue.main.async {
+                    completion(success)
+                }
+            })
+            dataTask?.resume()
         }
     }
     
